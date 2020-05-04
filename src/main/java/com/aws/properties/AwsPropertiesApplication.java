5cd50556b366834250dcb573221bf4af700c1e53
GP <gangadharind@gmail.com>
2020-05-03 21:08:56 -0400

    initial commit

---
+++ src/main/java/com/aws/properties/AwsPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.aws.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/properties/AwsPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
